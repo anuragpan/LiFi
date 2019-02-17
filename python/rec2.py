d1e90fff28f02091e2831edddff489058dc3a15d
Vishnuparammal <vishnu.130599@gmail.com>
2019-02-17 20:49:51 +0530

    text file transfer

--- python/rec2.py
+++ python/rec2.py
@@ -1,9 +1,9 @@
 ## import the serial library
 import serial
-start = False
 
 ## Boolean variable that will represent 
 ## whether or not the arduino is connected
+connected = False
 
 ## establish connection to the serial port that your arduino 
 ## is connected to.
@@ -13,10 +13,16 @@ locations=['/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3']
 for device in locations:
     try:
         print "Trying...",device
-        ser = serial.Serial(device, 9600, timeout=1)
+        ser = serial.Serial(device, 9600)
         break
     except:
         print "Failed to connect on",device
+
+## loop until the arduino tells us it is ready
+while not connected:
+    if ser.read() is '#':
+        connected = True
+        ser.write("#")
 
 ## open text file to store the current 
 ##gps co-ordinates received from the rover    
@@ -26,19 +32,12 @@ text_file = open("newFile.txt", 'w')
 while 1:
     if ser.inWaiting():
         x=ser.read()
-        if x is '$' :
-        	break 
-        if start:
-	        text_file.write(x)
-	        # if x=="\n":
-	        #      text_file.seek(0)
-	        #      text_file.truncate()
-	        text_file.flush()
-        if x is '~' :
-        	start = True
-        print(x)
-        
-        
+        print(x) 
+        text_file.write(x)
+        # if x=="\n":
+        #      text_file.seek(0)
+        #      text_file.truncate()
+        text_file.flush()
 
 ## close the serial connection and text file
 text_file.close()
--- python/trans.py
+++ python/trans.py
@@ -1,7 +1,15 @@
 import serial
-ser=serial.Serial('/dev/ttyUSB0',19200,timeout=0)
+import time
+ser=serial.Serial('/dev/ttyUSB0',9600,timeout=0)
+
+
 f=open("lifi.txt","r")
 f1=f.readlines()
 while True:
-    for x in f1:
-         ser.write(x)
+	ser.write("~")
+	for x in f1:
+		ser.write(x)
+	ser.write("$")
+
+
+	
